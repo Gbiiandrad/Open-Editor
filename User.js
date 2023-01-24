b7481d4766d75ebc5514ef14347a61a70254d9e2
Gabrielle Andrade Da Silva <105249937+Gbiiandrad@users.noreply.github.com>
2023-01-24 01:32:23 -0300

    Concluído ✔ - Classes privadas, Getts e Setts

--- User.js
+++ User.js
@@ -2,16 +2,64 @@
 // Não esquecer de criar o pack JSON ---- * npm init -y * E colocar o *"type": "module",* no arquivo.
 
 export default class User {
+    //private
+    #nome
+    #email
+    #nascimento
+    #role
+    #ativo
+
     constructor(nome, email, nascimento, role, ativo = true) {
-        this.nome = nome;
-        this.email = email;
-        this. nascimento = nascimento;
-        this.role = role || 'estudante'; // Ou ele vai iniciar como estudante
-        this.ativo = ativo;
+        this.#nome = nome; // # para private
+        this.#email = email;
+        this.#nascimento = nascimento;
+        this.#role = role || 'estudante'; // Ou ele vai iniciar como estudante
+        this.#ativo = ativo;
     }
+
+    get nome() {
+        return this.#nome;
+    }
+
+    get email() {
+        return this.#email;
+    }
+
+    get nascimento() {
+        return this.#nascimento;
+    }
+
+    get role() {
+        return this.#role;
+    }
+
+    get ativo() {
+        return this.#ativo;
+    }
+
+    set nome(novoNome) {
+        if(novoNome == '') {
+            throw new Error('Formato não VÁLIDO'); // dar erro
+        }
+        
+        this.#nome = novoNome;
+    }
+
+
+   /*  //metodo privado
+    #montaObjUser() {
+        return ({
+            nome: this.#nome,
+            email: this.#email,
+            nascimento: this.#nascimento,
+            role: this.#role,
+            ativo: this.#ativo
+        })
+    } */
 
     //metodos
     exibirInfos() {
-        return `Nome: ${this.nome} - Email: ${this.email}`;
+        //para mandar as informação para fora da classe provada
+        return `${this.nome} - ${this.email} - ${this.nascimento} - ${this.role} - ${this.ativo}`;
     }
 }
