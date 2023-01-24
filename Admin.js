a84983af08b6bd51c3dc326e3158c0a015e44f1f
Gabrielle Andrade Da Silva <105249937+Gbiiandrad@users.noreply.github.com>
2023-01-24 00:02:54 -0300

    parte 2 - permissao do Admin criar cursos

--- Admin.js
+++ Admin.js
@@ -4,9 +4,16 @@ class Admin extends User {
     constructor(nome, email, nascimento, role = 'Admin', ativo = true) { 
         super(nome, email, nascimento, role, ativo); // O SUPER é para aproveitar todas a propriedades da class User.
     }
+
+    //permitir que o Admin possa criar cursos
+    criarCursos(nomeDoCurso, vagas) {
+        return `Curso de ${nomeDoCurso} com ${vagas} vagas criado com SUCESSO!`;
+    }
 }
 
 const novoAdmin = new Admin ('Rodrigo', 'r@r.com', '2023-01-01');
 
-console.log(novoAdmin);
-console.log(novoAdmin.exibirInfos());
+console.log(novoAdmin.criarCursos('JS', 40));
+
+/* console.log(novoAdmin);
+console.log(novoAdmin.exibirInfos()); */
