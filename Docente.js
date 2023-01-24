471a9cb4e1cd38d3df8566e272c52ad7898c8e5c
Gabrielle Andrade Da Silva <105249937+Gbiiandrad@users.noreply.github.com>
2023-01-24 01:31:17 -0300

    Concluído ✔

--- Docente.js
+++ Docente.js
@@ -1,6 +1,6 @@
 import User from "./User.js";  // SEMPRE colocar o js no final *"./User.js";*
 
-class Docente extends User {
+export default class Docente extends User {
     constructor(nome, email, nascimento, role = 'docente', ativo = true) { 
         super(nome, email, nascimento, role, ativo); // O SUPER é para aproveitar todas a propriedades da class User.
     }
@@ -10,9 +10,3 @@ class Docente extends User {
         return `Estudante ${estudante} passou no curso de ${cursos}.`
     }
 }
-
-const novoDocente = new Docente ('Mariana', 'm@m.com', '2023-01-16');
-
-console.log(novoDocente);
-console.log(novoDocente.exibirInfos());
-console.log(novoDocente.aprovaEstudante('Juliana', 'JS'));
