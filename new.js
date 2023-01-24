f10817ae8280034fa61571d520bc4dfa422d759a
Gabrielle Andrade Da Silva <105249937+Gbiiandrad@users.noreply.github.com>
2023-01-23 23:16:42 -0300

    Parte 1 - Function com Construtor

---
+++ new.js
@@ -0,0 +1,26 @@
+//classes
+function User (nome, email) {
+    this.nome = nome;
+    this.email = email;
+
+    this.exibirInfos = function () {
+        return `${this.nome}, ${this.email}`;
+    }
+}
+
+// USANDO O CONSTRUTOR:
+/* const novoUser = new User('Juliana', 'j@j.com');
+console.log (novoUser.exibirInfos()); */
+
+
+// USANDO O CREATE:
+function Admin (role) {
+    User.call(this, 'Juliana', 'j2j.com');
+    this.role = role || 'estudante';
+}
+
+Admin.prototype = Object.create(User.prototype);
+const novoUser = new Admin ('Admin');
+
+console.log(novoUser.exibirInfos());
+console.log(novoUser.role);
