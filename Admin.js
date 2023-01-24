c68bffba5d580558543ed953406cdb625ff19045
Gabrielle Andrade Da Silva <105249937+Gbiiandrad@users.noreply.github.com>
2023-01-24 01:29:05 -0300

    Concluído ✔

--- Admin.js
+++ Admin.js
@@ -1,6 +1,6 @@
 import User from "./User.js";  // SEMPRE colocar o js no final *"./User.js";*
 
-class Admin extends User {
+export default class Admin extends User {
     constructor(nome, email, nascimento, role = 'Admin', ativo = true) { 
         super(nome, email, nascimento, role, ativo); // O SUPER é para aproveitar todas a propriedades da class User.
     }
@@ -11,9 +11,9 @@ class Admin extends User {
     }
 }
 
-const novoAdmin = new Admin ('Rodrigo', 'r@r.com', '2023-01-01');
+/* const novoAdmin = new Admin ('Rodrigo', 'r@r.com', '2023-01-01');
 
 console.log(novoAdmin.criarCursos('JS', 40));
-
+ */
 /* console.log(novoAdmin);
 console.log(novoAdmin.exibirInfos()); */
