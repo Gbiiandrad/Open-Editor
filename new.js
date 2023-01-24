3702d19090ff23bcbef728eb227388e9c39b735c
Gabrielle Andrade Da Silva <105249937+Gbiiandrad@users.noreply.github.com>
2023-01-23 23:25:54 -0300

    parte 3 - User em formato d objeto

--- new.js
+++ new.js
@@ -1,4 +1,4 @@
-//classes
+/* //classes
 function User (nome, email) {
     this.nome = nome;
     this.email = email;
@@ -6,14 +6,14 @@ function User (nome, email) {
     this.exibirInfos = function () {
         return `${this.nome}, ${this.email}`;
     }
-}
+} */
 
 // USANDO O CONSTRUTOR:
 /* const novoUser = new User('Juliana', 'j@j.com');
 console.log (novoUser.exibirInfos()); */
 
 
-// USANDO O CREATE:
+/* // USANDO O CREATE:
 function Admin (role) {
     User.call(this, 'Juliana', 'j2j.com');
     this.role = role || 'estudante';
@@ -24,4 +24,23 @@ const novoUser = new Admin ('Admin');
 
 
 console.log(novoUser.exibirInfos());
-console.log(novoUser.role);
+console.log(novoUser.role); */
+
+
+// User em formato de objeto:
+const user = {
+    init: function(nome, email) {
+        this.nome = nome;
+        this.email = email;
+    },
+
+    exibirInfos: function() {
+        return this.nome;
+    }
+}
+
+const novoUser = Object.create(user);
+novoUser.init('Juliana', 'j@j.com');
+
+console.log(novoUser.exibirInfos());
+//console.log(user.isPrototypeOf(novoUser)); 
